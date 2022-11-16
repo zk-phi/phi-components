daa19ea873ef554e6f5d7bfde28886434d981df6
zk-phi <zk-phi@users.noreply.github.com>
2022-11-16 22:32:42 +0900

    Export Icon component

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import Button from "./components/Button/";
-import Input from "./components/Input/";
+import Button from "./components/Button";
+import Input from "./components/Input";
+import Icon from "./components/Icon";
 
 const register = () => {
   Button.register();
   Input.register();
+  Icon.register();
 };
 
-export { Button, Input, register };
+export { Button, Input, Icon, register };
