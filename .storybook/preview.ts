0dbb447cddf1d4397d36e1963787a7273d5ee044
zk-phi <zk-phi@users.noreply.github.com>
2025-01-01 03:57:26 +0900

    Tweak base styles

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,7 @@
 import type { Preview } from "@storybook/react";
 import "../src/theme.css";
+import "../src/base.css";
+import "../contrib/destyle.min.css";
 
 const preview: Preview = {
   parameters: {
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 import "./theme.css";
+import "./base.css";
+import "../contrib/destyle.min.css";
 export { default as Button } from "./components/Button";
