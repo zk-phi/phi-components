dd0ac9234e71f9889ea37eb7bc3ac90498c79334
zk-phi <zk-phi@users.noreply.github.com>
2025-05-25 01:11:58 +0900

    Export some more components

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ import "./theme.css";
 import "./base.css";
 import "../contrib/destyle.min.css";
 export { default as Button } from "./components/Button";
+export { default as IconButton } from "./components/IconButton";
+export { default as TextButton } from "./components/TextButton";
