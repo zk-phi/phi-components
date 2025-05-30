7be5125eb2491907c626aa1b3207f312c0fa96da
zk-phi <zk-phi@users.noreply.github.com>
2025-05-31 02:52:54 +0900

    Generate type declarations with vite-plugin-dts

--- vite.custom-elements.config.ts
+++ vite.custom-elements.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite"
 import preact from "@preact/preset-vite"
+import dts from "vite-plugin-dts";
 import { analyzer } from "vite-bundle-analyzer";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     preact(),
+    dts(),
     analyzer({
       analyzerMode: "static",
       fileName: "stats.custom-elements.html",
