28f5fb130554667b744a020a61398ebe481fdade
zk-phi <zk-phi@users.noreply.github.com>
2025-06-18 15:15:40 +0900

    Update back/next icon

--- src/components/others/Icon/icons/Back.tsx
+++ src/components/others/Icon/icons/Back.tsx
@@ -1,6 +1,6 @@
 const Back = () => (
-  <svg class="phi-icon" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-    <path d="M640-80 240-480l400-400 71 71-329 329 329 329-71 71Z"/>
+  <svg class="phi-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
+    <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z"/>
   </svg>
 );
 
--- src/components/others/Icon/icons/Next.tsx
+++ src/components/others/Icon/icons/Next.tsx
@@ -1,6 +1,6 @@
 const Next = () => (
   <svg class="phi-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
-    <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z"/>
+    <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/>
   </svg>
 );
 
