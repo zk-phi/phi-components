412f347bda1c0923cb6cff22ff3998e4ab279931
zk-phi <zk-phi@users.noreply.github.com>
2025-06-18 15:15:18 +0900

    Refactoring: Remove unused types

--- src/components/others/Icon/icons/Effect.tsx
+++ src/components/others/Icon/icons/Effect.tsx
@@ -1,7 +1,3 @@
-type Props = {
-  class: string,
-};
-
 const Effect = () => (
   <svg class="phi-icon" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24">
     <g>
